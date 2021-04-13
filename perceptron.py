fa8ac874dd7ef7d75cc6daa7365651650c0d4234
reurison-sr <43077478+reurison-sr@users.noreply.github.com>
2021-04-13 17:13:20 -0300

    Update perceptron.py
    Update Perceptron Simple and add Perceptron Linear

--- perceptron.py
+++ perceptron.py
@@ -3,56 +3,96 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
-def Entrenar_Perceptron(factor_aprendizaje, X_train, Y_train, epochs):
+def Entrenar_Perceptron_Simple(factor_aprendizaje, X_train, Y_train, epochs):
 
     ''' Algoritmo perceptron simple (diapositiva 40/60)
-    Retorna los parametros del hiperplano (w0, w1, w2) -> w0*x + w1*y + w2 = 0'''
-    
+    Retorna los parametros del hiperplano (w0, w1,..., w[n-1]) -> w0*x + w1*y + w2*z + ... + w[n-1] = 0'''
+
     i = 0 
-    w = np.random.rand(2, 1) #w1, w2 (Problemas en 2D)
-    b = 0
+    w_shape = X_train.shape[1]
+    w = np.zeros((w_shape+1, 1))
     error = 1
     error_min = 2*len(X_train)
     
     while error > 0 and i < epochs:
-        
+       
         i_x = X_train.sample() #saco 1 elemento aleatório del conjunto de entrenamiento.
-        x = np.array([ i_x['xi'], i_x['yi'] ]) #(xi, yi)
-        index = int(i_x.index.values)
-        ClaseReal = Y_train[index]
+        x = np.zeros(w.shape) #(xi, yi, zi,..., 1) (x : ksi_{i}^{u})
+        x[-1] = 1
+        x[:-1] = np.array(i_x.T)
+
+        index   = int(i_x.index.values)
+        zeta_mu = float(Y_train[index]) # Salida deseada
+        O_mu    = float(np.dot(x.T, w)) # Excitacion
+        activacao = 1 if O_mu >= 0 else -1 #Funcion Escalon
         
-        exitacion = np.dot(x.T, w) + b
-        activacao = 1 if exitacion >= 0 else -1 #Funcion Escalon
-        
-        #Actualizacion de los pesos
-        err = ClaseReal - activacao
-        delta_w = factor_aprendizaje*(err)*x
-        delta_b = factor_aprendizaje*(err)
-        w = w + delta_w
-        b = b + delta_b
+        #Actualizacion de los pesos (diapositiva 36/60)
+        delta     = (factor_aprendizaje*(zeta_mu - activacao)*x
+        w         = w + delta
         
         #Calculo del error sobre todo el conjunto de entrenamiento
-        error = CalcularErro(X_train, Y_train, w, b)
+        error = ErrorSimple(X_train, Y_train, w)
         if error < error_min:
             error_min = error
             w_min = w
-            b_min = b
         i += 1
         
-    return w_min[0], w_min[1], b_min
+    return w_min, error_min
 
 
-def CalcularErro(X_train, Y_train, w, b):
-    Ctd_erros = 0 #Cantidad de errores de clasificación con los parametros w y b.
+def ErrorSimple(X_train, Y_train, w):
+    Ctd_erros = 0 
     for index, row in X_train.iterrows():
-        x = np.array([row[0], row[1]]) #(xi, yi)
-        ClaseReal = Y_train[index] #Clase real
-        exitacion = np.dot(x.T, w) + b
-        activacao = 1 if exitacion >= 0 else -1  #clasificacao perceptron
-        error = ClaseReal - activacao
+        x = np.zeros((X_train.shape[1]+1, 1)) #(xi, yi, zi,...., 1) (x : ksi_{i}^{u})
+        x[-1] = 1
+        rows = np.array([float(row[i]) for i in range(X_train.shape[1])])
+        rows = rows.reshape((X_train.shape[1], 1))
+        x[:-1] = rows
+        zeta_mu = Y_train[index] #Salida deseada
+        O_mu = np.dot(x.T, w)
+        activacao = 1 if O_mu >= 0 else -1  #clasificacao perceptron
+        error = zeta_mu - activacao
         if (error != 0):
             Ctd_erros += 1
     return Ctd_erros
+
+
+def Entrenar_Perceptron_Lineal(factor_aprendizaje, X_train, Y_train, epochs):
+
+    '''Algoritmo para entrenar el perceptron simple 
+        Retorna los parametros del hiperplano (w0, w1,..., w[n-1]) -> w0*x + w1*y + w2*z + ... + w[n-1] = 0'''
+    
+    i = 0
+    w_shape = X_train.shape[1]
+    w = np.zeros((w_shape+1, 1))
+    b = 0
+    J = []
+    
+    while i < epochs:
+
+        errors = 0
+
+        for index, row in X_train.iterrows():
+            #Para cada elemento do conjunto de treinamento
+            i_x = X_train.sample() #saco 1 elemento aleatório del conjunto de entrenamiento.
+            x = np.zeros(w.shape) #(xi, yi, zi,..., 1) (x : ksi_{i}^{u})
+            x[-1] = 1
+            x[:-1] = np.array(i_x.T)
+            zeta_mu = float(Y_train[index]) #Salida deseada
+            O_mu = float(np.dot(x.T, w))
+
+            #Actualizacion de los pesos (diapositiva 36/60)
+            erro      = zeta_mu - O_mu
+            delta     = (factor_aprendizaje*erro)*x
+            w         = w + delta
+            errors += (erro**2)
+
+        J.append(errors)
+
+        i += 1
+
+    return w, J # w = (w0, w1,..., w[n-1]) , donde w[n-1] es el umbral.
+
 
 
 def ReadFiles():
@@ -85,55 +125,92 @@ def ReadFiles():
 
 
 if __name__ == "__main__":
-    # Leer Archivo
 
-    data = ReadFiles()
-    X_data = data.iloc[:,0:3]   # [xi, yi, zi]
-    Clase_data = data.iloc[:,3] # [clase real]
-    #Separacion entre conjunto de entrenamiento y testeo
-    test_size = 20 #Porcentaje de elementos en el conjunto de testeo
-    X_train, X_test, y_train, y_test = train_test_split(X_data, Clase_data, test_size = 0)
-    factor_aprendizaje = 0.2
+    #--Leer Archivo
+    #data = ReadFiles()
+    #X_data = data.iloc[:,0:3]   # [xi, yi, zi]
+    #Clase_data = data.iloc[:,3] # [clase real o clase deseada]
+    #--Separacion entre conjunto de entrenamiento y testeo
+    #X_train, X_test, y_train, y_test = train_test_split(X_data, Clase_data, test_size = 0.2)
+    #factor_aprendizaje = 0.0001
+    #epochs = 100
+    #w0, w1, w2, J = Entrenar_Perceptron_Lineal(factor_aprendizaje, X_train, y_train, epochs)
+    #print('Parametros del hiperplano')
+    #print('w0 : {0}'.format(w0))
+    #print('w1 : {0}'.format(w1))
+    #print('w2 : {0}'.format(w2))
+    #print('J : {0}'.format(J))
+
+
+    #-----------------------------------Exemplo Diapositivas----------------------------------------
+    xi = [1.1946, 0.8788, 1.1907, 1.4180, 0.2032, 2.7571, 4.7125, 3.9392, 1.2072, 3.4799, 0.4763]
+    yi = [3.8427, 1.6595, 1.6117, 3.8272, 1.9208, 1.0931, 2.8166, 1.1032, 0.8132, 1.9982, 0.1020]
+    Salida  = [1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1]
+    colores = ['red','red','red','red','red','blue','blue','blue','blue','blue','blue']
+    Data    = pd.DataFrame({'xi':xi, 'yi':yi, 'Salida':Salida})
+    X_data  = Data.iloc[:,0:2]
+    Clase_data = Data.iloc[:,2]
+    X_train, X_test, y_train, y_test = train_test_split(X_data, Clase_data, test_size=0)
+    factor_aprendizaje = 0.01
     epochs = 150
-    #w0, w1, w2 = Entrenar_Perceptron(factor_aprendizaje, X_train, y_train, epochs)
+    W, error_min = Entrenar_Perceptron_Simple(factor_aprendizaje, X_train, y_train, epochs)
+    w0 = W[0]
+    w1 = W[1]
+    w2 = W[2]
+    print('Parametros del hiperplano')
+    print('w0 : {0}'.format(w0))
+    print('w1 : {0}'.format(w1))
+    print('w2 : {0}'.format(w2))
+    print('error : {0}'.format(error_min))
+    #----------------------------------------------------------------------------------------------
+
 
     #-------------------------------------Problema 1-------------------------------------
-    xi = [-1,+1,-1,+1]
-    yi = [+1,-1,-1,+1]
-    Salida_AND = [-1,-1,-1,+1] 
-    Salida_XOR = [+1,+1,-1,-1]
+    #xi = [-1,+1,-1,+1]
+    #yi = [+1,-1,-1,+1]
+    #Salida_AND = [-1,-1,-1,+1] 
+    #Salida_XOR = [+1,+1,-1,-1]
+    #set_AND = pd.DataFrame({'xi':xi, 'yi':yi, 'Salida':Salida_AND})
+    #set_XOR = pd.DataFrame({'xi':xi, 'yi':yi, 'Salida':Salida_XOR})
 
-    set_AND = pd.DataFrame({'xi':xi, 'yi':yi, 'Salida':Salida_AND})
-    set_XOR = pd.DataFrame({'xi':xi, 'yi':yi, 'Salida':Salida_XOR})
+    #X_AND = set_AND.iloc[:,0:2] # [xi, yi]
+    #Clase_AND = set_AND.iloc[:,2] # [clase real]
+    #X_XOR = set_XOR.iloc[:,0:2]
+    #Clase_XOR = set_XOR.iloc[:,2]
 
-    X_AND = set_AND.iloc[:,0:2] # [xi, yi]
-    Clase_AND = set_AND.iloc[:,2]   # [clase real]
+    #--Separacion entre conjunto de entrenamiento y testeo
+    #X_train, X_test, y_train, y_test = train_test_split(X_AND, Clase_AND, test_size = 0)
 
-    X_XOR = set_XOR.iloc[:,0:2]
-    Clase_XOR = set_XOR.iloc[:,2]
+    #--Condiciones Iniciales
+    #factor_aprendizaje = 0.2
+    #epochs = 150
+    #W, error_min = Entrenar_Perceptron_Simple(factor_aprendizaje, X_train, y_train, epochs)
+    #w0 = W[0]
+    #w1 = W[1]
+    #w2 = W[2]
+    #print('Parametros del hiperplano')
+    #print('w0 : {0}'.format(w0))
+    #print('w1 : {0}'.format(w1))
+    #print('w2 : {0}'.format(w2))
+    #print('error : {0}'.format(error_min))
+    #-----------------------------------------------------------------------------------
 
-    #Separacion entre conjunto de entrenamiento y testeo
-    test_size = 0 #Porcentaje de elementos en el conjunto de testeo
-    X_train, X_test, y_train, y_test = train_test_split(X_AND, Clase_AND, test_size = 0)
 
-    # Condiciones Iniciales
-    factor_aprendizaje = 0.5
-    epochs = 150
-    w0, w1, w2 = Entrenar_Perceptron(factor_aprendizaje, X_train, y_train, epochs)
+    #-----------------------------------PLOT 2D-----------------------------------------
+    #Solo funciona para el ejemplo del perceptron simple
 
-    #x.w0 + y.w1 + w2 = 0
-    #y.w1 = -w0.x - w2
-    #y    = -(w0/w1)x - (w2/w1) --> Recta en el R².
-    #m = -w0/w1; b = -w2/w1
+    #--x.w0 + y.w1 + w2 = 0
+    #--y.w1 = -w0.x - w2
+    #--y    = -(w0/w1)x - (w2/w1) --> Recta en R².
+    #--m = -w0/w1; b = -w2/w1
     m = -(w0/w1)
     b = -(w2/w1)
-    x = np.linspace(-1.5, 1.5, 100) 
+    x = np.linspace(-5, 5, 100) 
     y_perceptron = m*x + b
 
     fig, ax = plt.subplots()
-    plt.style.use('seaborn-whitegrid')
-    colorsAND = ['red','red','red','blue']
-    colorsXOR = ['blue','blue','red','red']
-    plt.scatter(xi, yi, c=colorsAND)
+    #colorsAND = ['red','red','red','blue']
+    #colorsXOR = ['blue','blue','red','red']
+    plt.scatter(xi, yi, c=colores)
     plt.plot(x, y_perceptron, color='green')
     plt.show()
